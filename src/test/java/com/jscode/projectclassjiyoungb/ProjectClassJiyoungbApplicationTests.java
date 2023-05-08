3d1d773c2057d64b7eea7b2fde54bfac1f90bb48
jiyoung-B <catgirl0313@naver.com>
2023-05-08 22:37:39 +0900

    first commit

---
+++ src/test/java/com/jscode/projectclassjiyoungb/ProjectClassJiyoungbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jscode.projectclassjiyoungb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectClassJiyoungbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
